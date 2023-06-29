6c16d44b987eb58cd9827c027b56dc84a904d4a9
Mani Heshmati <maniheshmati1385@gmail.com>
2023-06-29 06:21:52 +0430

    Initial installation pushed to repo

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        
+        #header{
+            background-color: #008080;
+            height: 100px;
+            width: 100%;
+            display: flex;
+        }
+        li{
+
+            display: inline;
+            padding: 10px;
+            color: white;
+            background-color: #008080;
+            font-size: 20px;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div id="header">
+    <li><a href="register">Register</a></li>
+    <li><a href="login">Login</a></li>
+    </div>
+</body>
+</html>
