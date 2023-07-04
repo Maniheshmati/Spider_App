84f008978065f7468c181bce56e4bdd2e5261a10
Mani Heshmati <maniheshmati1385@gmail.com>
2023-07-04 15:38:18 +0430

    Now the error messages while registering wll show for user

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,19 +11,22 @@
       <div class="mb-3">
         <label for="email" class="form-label">Email address</label>
         <input type="email" class="form-control" id="email" name="email" required>
+          {{$errors->first('email')}}
       </div>
       <div class="mb-3">
         <label for="username" class="form-label">Username</label>
         <input type="text" class="form-control" id="username" name="username" required>
+          {{$errors->first('username')}}
       </div>
       <div class="mb-3">
         <label for="password" class="form-label">Password</label>
         <input type="password" class="form-control" id="password" name="password" required>
-        
+          {{$errors->first('password')}}
+
       </div>
       <button type="submit" class="btn btn-primary">Register</button>
     </form>
   </div>
-  
+
 
 @include('layouts.footer')
