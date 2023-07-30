65f245c087dcfd9b56066fa99dda01038df285ae
Maniheshmati <maniheshmati1385@gmail.com>
2023-07-30 12:25:25 +0330

    Header UI Changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,9 +8,9 @@
 </head>
 <body>
   <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #d83c3c">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Post Management</a>
+        <a class="navbar-brand" href="#">Spider Man </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
