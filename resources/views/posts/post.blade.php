c4c31983951f3976b82011804d92784a1c2e1467
Maniheshmati <maniheshmati1385@gmail.com>
2023-07-30 11:16:50 +0330

    Now the website checks if user was logged in the can add comments else user cannot see comment form

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -8,12 +8,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$post->title}}</title>
     <style>
+        /* Add your custom styles here */
+
         body {
             background-color: #f1f1f1;
             font-family: Arial, sans-serif;
-            padding: 20px;
+            /* padding: 20px; */
             margin: 0;
         }
+
         .container {
             max-width: 800px;
             margin: 0 auto;
@@ -23,18 +26,52 @@
             border-radius: 5px;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
-        .table {
-            width: 100%;
-            border-collapse: collapse;
+
+        .post-card {
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
             margin-bottom: 20px;
         }
-        .table th, .table td {
-            border: 1px solid #ccc;
-            padding: 8px;
-            text-align: left;
+
+        .post-title {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 10px;
         }
-        .btn {
-            display: inline-block;
+
+        .post-body {
+            margin-bottom: 20px;
+        }
+
+        .comment-card {
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            margin-bottom: 10px;
+        }
+
+        .comment-author {
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        .comment-body {
+            margin-bottom: 5px;
+        }
+
+        .comment-time {
+            font-size: 12px;
+            color: #777;
+        }
+
+        .actions-container {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 20px;
+        }
+
+        .action-btn {
             padding: 8px 12px;
             background-color: #007bff;
             color: #fff;
@@ -44,79 +81,45 @@
             text-decoration: none;
             font-size: 16px;
         }
-        .btn-primary {
+
+        .action-btn-primary {
             background-color: #007bff;
         }
-        .btn-primary:hover {
+
+        .action-btn-primary:hover {
             background-color: #0056b3;
-        }
-        .label {
-            font-weight: bold;
-            margin-bottom: 5px;
-        }
-        .form-control {
-            width: 100%;
-            padding: 8px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            resize: vertical;
-        }
-        .comment {
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin-bottom: 10px;
-            border-radius: 5px;
-        }
-        .comment-author {
-            font-weight: bold;
-            margin-bottom: 5px;
-        }
-        .comment-body {
-            margin-bottom: 5px;
-        }
-        .comment-time {
-            font-size: 12px;
-            color: #777;
         }
     </style>
 </head>
 <body>
     <div class="container">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Body</th>
-                    <th>Created at</th>
-                    <th>Updated at</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{$post->id}}</td>
-                    <td>{{$post->title}}</td>
-                    <td>{{$post->body}}</td>
-                    <td>{{$post->created_at}}</td>
-                    <td>{{$post->updated_at}}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="post-card">
+            <div class="post-title">{{$post->title}}</div>
+            <div class="post-body">{{$post->body}}</div>
+        </div>
 
+        @if (auth()->check())
         @if (Auth::user()->id == $post->user_id OR Auth::user()->hasRole('owner') OR Auth::user()->hasRole('admin'))
-            <a href="{{ route('posts.create', ['id' => $post->id]) }}"><button class="btn btn-primary">Create Post</button></a>
-            <a href="{{ route('posts.update', ['id' => $post->id]) }}"><button class="btn btn-primary">Update Post</button></a>
-            <a href="{{ route('posts.delete', ['id' => $post->id]) }}"><button class="btn btn-primary">Delete Post</button></a>
-            <a href="/"><button class="btn btn-primary">Search in posts</button></a>
+            <div class="actions-container">
+                <a href="{{ route('posts.create', ['id' => $post->id]) }}" class="action-btn action-btn-primary">Create Post</a>
+                <a href="{{ route('posts.update', ['id' => $post->id]) }}" class="action-btn action-btn-primary">Update Post</a>
+                <a href="{{ route('posts.delete', ['id' => $post->id]) }}" class="action-btn action-btn-primary">Delete Post</a>
+                <a href="/" class="action-btn action-btn-primary">Search in posts</a>
+            </div>
         @endif
-
-        <form method="POST"">
-            @csrf
-            <label for="body" class="label"> Comment:</label>
-            <textarea name="body" class="form-control" required></textarea>
-            <input type="hidden" name="post_id" value="{{$post->id}}">
-            <input type="submit" value="Comment" class="btn btn-primary">
-        </form>
+    @endif
+    
+        
+        
+            @if (Auth::check())
+            <form method="POST">
+                @csrf
+                <label for="body" class="label">Comment:</label>
+                <textarea name="body" class="form-control" required></textarea>
+                <input type="hidden" name="post_id" value="{{$post->id}}">
+                <input type="submit" value="Comment" class="action-btn action-btn-primary">
+            </form>
+            @endif
 
         <h2>Comments</h2>
         @php
@@ -128,9 +131,7 @@
             $commentTime = \Carbon\Carbon::parse($comment['created_at'])->diffForHumans();
 
         @endphp
-        
-        
-            <div class="comment">
+            <div class="comment-card">
                 <div class="comment-author">{{ $user->name }}</div>
                 <div class="comment-body">{{ $comment['body'] }}</div>
                 <div class="comment-time">{{ $commentTime }}</div>
